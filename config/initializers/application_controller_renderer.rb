6f3296c1f3cd8c82a697dcb6bcab4a09a5ff220f
rina-1 <cba.mac@icloud.com>
2020-04-20 07:54:35 +0900

    [Finish]5-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,4 +1,4 @@
-# Be sure to restart your server when you modify this file.
+
 
 # ActiveSupport::Reloader.to_prepare do
 #   ApplicationController.renderer.defaults.merge!(
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'a110b5cff7241930e944df67b3771111b1a4660cc3258e8388d2672a50d35f3a2533e074355602aecca254a297d272c10736b5b041eb23c0ec35d7c51465215b'
