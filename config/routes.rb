06bee80f5265042e85bb943862273a7b20f3ed7e
rina-1 <cba.mac@icloud.com>
2020-04-20 10:27:03 +0900

    [Finish]5-6

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
-resources :users, only: [:show]
+resources :users, only: [:show,:edit,:update]
 end
